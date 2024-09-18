7f57023d93f6604781d804257cd472ccb9bd54f7
Vipanpreet Kaur <sohivipan@gmail.com>
2024-09-18 16:34:13 -0700

    Finished

--- scripts/index.js
+++ scripts/index.js
@@ -51,7 +51,7 @@ function closePopop() {
 function getCardElement(cardData) {
   const cardElement = cardTemplate.cloneNode(true);
   const cardImageEl = cardElement.querySelector(".card__image");
-  const cardTitleEl = cardElement.querySelector(".card__title");
+  const cardTitleEl = cardElement.querySelector(".card__description-title");
   cardImageEl.setAttribute("src", cardData.link);
   cardImageEl.setAttribute("alt", cardData.name);
   cardTitleEl.textContent = cardData.name;
